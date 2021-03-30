ad77ec692e32adbabda9263b5fb6416a09394c87
noobsauraj <78680348+noobsauraj@users.noreply.github.com>
2021-03-30 11:54:26 +0530

    Update and rename havoc_RMX2151.mk to dot_RMX2151.mk

--- dot_RMX2151.mk
+++ dot_RMX2151.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2151/device.mk)
 
 # Inherit some common Lineage OS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_RMX2151
+PRODUCT_NAME := dot_RMX2151
 PRODUCT_DEVICE := RMX2151
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G95 Series
