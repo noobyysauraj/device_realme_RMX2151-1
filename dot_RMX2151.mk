bd89a94b08377dbc025badc6216740ae365081a7
noobyysauraj <81681419+noobyysauraj@users.noreply.github.com>
2021-04-01 23:09:45 +0530

    Update dot_RMX2151.mk

--- dot_RMX2151.mk
+++ dot_RMX2151.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2151/device.mk)
 
 # Inherit some common Lineage OS stuff.
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 # Device identifier. This must come after all inclusions.
